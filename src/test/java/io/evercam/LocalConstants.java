8b6d5c7a8c075f57fc19723eda31849108dc7d27
Liuting Du <kangtadlt@gmail.com>
2016-04-27 11:32:00 +0100

    Fixed Travis build

---
+++ src/test/java/io/evercam/LocalConstants.java
@@ -0,0 +1,15 @@
+package io.evercam;
+
+public class LocalConstants {
+    public static String IP = "89.101.123.123";
+    public static String USERNAME = "username";
+    public static String PASSWORD = "password";
+    public static int HTTP_PORT = 80;
+    public static int RTSP_PORT = 554;
+    public static String SNAPSHOT_ENDING = "/Streaming/Channels/1/Picture";
+
+    public static final String EVERCAM_USERNAME = "dummyname";
+    public static final String TEST_API_KEY = "key";
+    public static final String TEST_API_ID = "id";
+    public static final String TEST_CAMERA_ID = "id";
+}
